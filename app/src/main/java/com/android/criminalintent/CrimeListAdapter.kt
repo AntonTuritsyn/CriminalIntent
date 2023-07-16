b9256b50792b1e51f30f102db82eaf3a9e2046f9
Anton_Turitsyn <turitsyn.anton@gmail.com>
2023-07-16 21:23:06 +0300

    First commit

---
+++ app/src/main/java/com/android/criminalintent/CrimeListAdapter.kt
@@ -0,0 +1,67 @@
+package com.android.criminalintent
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.android.criminalintent.databinding.ListItemCrimeBinding
+
+/*
+* RecyclerView никогда не создает представление (View)
+* Для этого всегда создается ViewHolder
+*/
+/*
+    Передаем корневое представление в качестве аргумента конструктору RecyclerView.ViewHolder
+    Затем базовый класс ViewHolder будет удерживать представление в свойстве itemView
+*/
+class CrimeHolder(
+    private val binding: ListItemCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
+        binding.root.setOnClickListener {
+            Toast.makeText(binding.root.context, "${crime.title} clicked!", Toast.LENGTH_SHORT)
+                .show()
+        }
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+}
+
+
+/*  CrimeListAdapter знает все детали об элементах в списке.
+    Адаптер также знает о списке преступлений, который поддерживает RecyclerView*/
+class CrimeListAdapter(
+    private val crimes: List<Crime>
+) : RecyclerView.Adapter<CrimeHolder>() {
+
+    /*  Adapter.onCreateViewHolder(...) отвечает за создание привязки к отображению, оболочку представления
+        в держателе представления (ViewHolder) и возврат результата.
+        Здесь "раздувается" (inflate) и привязывается ListItemCrimeBinding и передается результирующая привязка (binding) новому экземпляру CrimeHolder.*/
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+        return CrimeHolder(binding)
+    }
+
+    /*  Adapter.onBindViewHolder(...) отвечает за заполнение данного держателя (holder) преступлением из заданной позиции.
+        В этом случае происходит получение преступления из списка по запрашиваемой позиции.
+        Затем используется заголовок и дата преступления, чтобы установить текстовую инф. в соответствующих текстовых представлениях.*/
+    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+        val crime = crimes[position]
+        /*holder.apply {
+            binding.crimeTitle.text = crime.title
+            binding.crimeDate.text = crime.date.toString()
+        }*/
+        holder.bind(crime)
+    }
+
+    /*  Сообщаем RecyclerView, сколько элементов находится в наборе данных
+        Здесь - количество элементов в заданном массиве (будет переделано)*/
+    override fun getItemCount() = crimes.size
+}
---
+++ app/src/main/java/com/android/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,68 @@
+package com.android.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment: Fragment() {
+
+    private var _binding: FragmentCrimeListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    private val crimeListViewModel: CrimeListViewModel by viewModels()
+
+//      Не нужно
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
+//      Установка LayoutManager для отображения элементов в RecyclerView. Используется LinearLayout для вертикального отображения
+        binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+//      После создания адаптера создаем экземпляр с данными о преступлении и подключаем его к RecyclerView.
+//      Удаляется после добавления repeatOnLifecycle в onViewCreated
+        /*val crimes = crimeListViewModel.crimes
+        val adapter = CrimeListAdapter(crimes)
+        binding.crimeRecyclerView.adapter = adapter*/
+
+        return binding.root
+    }
+/*      Ваш код будет вести себя точно так же, как и с onStart() и onStop(), но теперь нужно переопределить меньше методов жизненного цикла, и вам не придется беспокоиться о том, что вы забудете отменить задание. repeatOnLifecycle(...) сделает все это за вас. repeatOnLifecycle(...) начнет выполнять код сопрограммы, когда фрагмент перейдет в запущенное состояние, и продолжит работу в возобновленном состоянии.        Но если ваше приложение работает в фоновом режиме и фрагмент больше не отображается, repeatOnLifecycle(...) отменит работу, как только фрагмент перейдет из начального состояния в созданное.        Если ваш жизненный цикл снова войдет в запущенное состояние без полного уничтожения, ваша сопрограмма будет перезапущена с самого начала, повторяя свою работу. (Это объясняет название функции.)                      Перед запуском приложения очистите ненужный код.        Удалите реализацию onCreate(...), которая регистрирует количество преступлений; Вам это больше не нужно.        Кроме того, удалите код, который пытается инициализировать CrimeListAdapter с отсутствующими данными.*/
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val crimes = crimeListViewModel.loadCrimes()
+                binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
